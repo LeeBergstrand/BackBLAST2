99dcb2c284f5d12916c19bdff585bffc4df20016
Lee Bergstrand <leemacboy@gmail.com>
2015-06-04 16:49:57 -0700

    Added function for parsing blast cvs results.
    Added limited testing for this function.

--- back_lib.py
+++ back_lib.py
@@ -1,11 +1,43 @@
 import csv
 import subprocess
 import sys
+from hashlib import md5
 from Bio import SeqIO
 
+# ==========
+# Classes:
+# ==========
 
+# -------------------------------------------------------------------------------------------------
+class HighScoringSegmentPair(object):
+	"""
+	Definition of a HighScoringSegmentPair.
+	:var query_seq_id: The query protein's sequence ID
+	:var sub_seq_id: The query protein's sequence ID
+	:var percent_identity: The percentage of identical amino acids between the two proteins.
+	:var evalue:    The expect value describes the number of hits one can "expect" to see by chance when searching
+					a database of a particular size.
+	:var coverage: The amount of aligned sequence overlap between the two proteins.
+	:var bitscore: A score of the quality of alignment between the two proteins which takes into account database size.
+	"""
+	def __init__(self, query_seq_id, sub_seq_id, percent_identity, evalue, coverage, bitscore):
+		self.query_seq_id = str(query_seq_id)
+		self.sub_seq_id = str(sub_seq_id)
+		self.percent_identity = float(percent_identity)
+		self.evalue = float(evalue)
+		self.coverage = float(coverage)
+		self.bitscore = float(bitscore)
+
+	def get_md5(self):
+		hash_string = "".join([str(x) for x in self.__dict__.values()])  # Join all attributes into a single string.
+		hash_md5 = md5(hash_string.encode('utf-8')).hexdigest()  # Create md5 hash.
+		return hash_md5
+
+# ==========
 # Functions:
-# =================================================================================================
+# ==========
+
+# -------------------------------------------------------------------------------------------------
 def run_blastp(query_file, database_file, e_value_cutoff, processes):
 	"""
 	Runs BLASTp...
@@ -26,8 +58,27 @@ def run_blastp(query_file, database_file, e_value_cutoff, processes):
 
 
 # -------------------------------------------------------------------------------------------------
+def create_hit_dict(blast_csv_in):
+	hit_dict = {}
+
+	blast_csv_in = blast_csv_in.splitlines(True)  # Converts raw BLAST csv output into list of csv rows.
+	blast_reader = csv.reader(blast_csv_in)  # Reads BLAST csv rows as a csv.
+
+	for row in blast_reader:
+		query_seq_id = row[0]
+		sub_seq_id = row[1]
+		percent_identity = row[2]
+		evalue = row[3]
+		coverage = row[4]
+		bitscore = row[5]
+
+		new_hsp = HighScoringSegmentPair(query_seq_id, sub_seq_id, percent_identity, evalue, coverage, bitscore)
+		hit_dict[new_hsp.get_md5()] = new_hsp
+
+	return hit_dict
+
 # 3: Filters HSPs by Percent Identity...
-def filterBLASTCSV(BLASTOut):
+def filter_blast_csv(BLASTOut):
 	minIdent = 25
 
 	BLASTCSVOut = BLASTOut.splitlines(True)  # Converts raw BLAST csv output into list of csv rows.
--- testing/test_BackBLAST.py
+++ testing/test_BackBLAST.py
@@ -1,16 +1,54 @@
 import unittest
 
-from ..back_lib import run_blastp
+from ..back_lib import run_blastp, create_hit_dict
+import csv
 
 
 class MyTestCase(unittest.TestCase):
 	def test_BLAST(self):
-		with open("./testing/test_files/expected_outputs/forward_blast_one.txt",'r') as blast_test_data:
+		with open("./testing/test_files/expected_outputs/forward_blast_one.txt", 'r') as blast_test_data:
 			reference_blast_results = blast_test_data.read()
 			reference_blast_results = reference_blast_results.replace(' ', '')
-			new_blast_results = run_blastp("./testing/test_files/test_queries/CommamonasCluster.faa", "./testing/test_files/test_databases/CP001220.1.faa", "1e-25", 1)
+			new_blast_results = run_blastp("./testing/test_files/test_queries/CommamonasCluster.faa",
+			                               "./testing/test_files/test_databases/CP001220.1.faa", "1e-25", 1)
 
 			self.assertEqual(new_blast_results, reference_blast_results)
+
+	def test_hit_loading(self):
+		with open("./testing/test_files/expected_outputs/forward_blast_one_hit.txt", 'r') as blast_test_data:
+			raw_hit_data = blast_test_data.read()
+			reference_blast_results = raw_hit_data.replace(' ', '')
+
+			blast_data_rows = reference_blast_results.splitlines(True)
+			blast_csv = csv.reader(blast_data_rows)
+
+			row = next(blast_csv)
+			test_query_seq_id = str(row[0])
+			test_sub_seq_id = str(row[1])
+			test_percent_identity = float(row[2])
+			test_evalue = float(row[3])
+			test_coverage = float(row[4])
+			test_bitscore = float(row[5])
+
+			hit_dict = create_hit_dict(reference_blast_results)
+			first_hit = hit_dict.popitem()[1]
+
+			pass_test = True
+
+			if first_hit.query_seq_id != test_query_seq_id:
+				pass_test = False
+			elif first_hit.sub_seq_id != test_sub_seq_id:
+				pass_test = False
+			elif first_hit.percent_identity != test_percent_identity:
+				pass_test = False
+			elif first_hit.evalue != test_evalue:
+				pass_test = False
+			elif first_hit.coverage != test_coverage:
+				pass_test = False
+			elif first_hit.bitscore != test_bitscore:
+				pass_test = False
+
+			self.assertTrue(pass_test)
 
 if __name__ == '__main__':
 	unittest.main()
